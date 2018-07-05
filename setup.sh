303634f5165fdaf37c6330d8986566053d5368fe
Jayson Grace <jayson.e.grace@gmail.com>
2018-07-04 23:34:04 -0700

    Fixed bug in setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update
 apt-cache policy docker-ce
 sudo apt install -y docker-ce
-sudo docker build . -t l50/rest-exp
+git clone git://github.com/l50/rest-experiment.git
+sudo docker build rest-experiment/ -t l50/rest-exp
 sudo docker run -d -p 80:80 --rm --name=rest-exp l50/rest-exp
